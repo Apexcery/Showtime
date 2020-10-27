27d84baecc612551ecd06143cdc60c1afb8377cc
Stuart Gleeson <stuart.gleeson98@gmail.com>
2020-10-27 23:47:27 +0000

    Created initial web application.

---
+++ Showtime.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Showtime.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
