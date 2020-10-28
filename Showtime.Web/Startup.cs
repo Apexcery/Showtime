69a820b7d41a48c83a032889960ebee59380fe36
Stuart Gleeson <stuart.gleeson98@gmail.com>
2020-10-28 00:46:10 +0000

    Removed unnecessary comments

--- Showtime.Web/Startup.cs
+++ Showtime.Web/Startup.cs
@@ -27,7 +27,6 @@ namespace Showtime.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(provider => Configuration);
@@ -35,7 +34,6 @@ namespace Showtime.Web
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,7 +43,7 @@ namespace Showtime.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
