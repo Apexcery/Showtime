527959a5a1cc1fc885a160415be12f2e96330849
Stuart Gleeson <stuart.gleeson98@gmail.com>
2021-08-20 02:14:46 +0100

    Created bottom navigation bar.

--- Showtime.Mob/MainActivity.cs
+++ Showtime.Mob/MainActivity.cs
@@ -1,13 +1,15 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
 using AndroidX.AppCompat.App;
 using AndroidX.AppCompat.Widget;
+using Google.Android.Material.BottomNavigation;
 
 namespace Showtime.Mob
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,6 +23,9 @@ namespace Showtime.Mob
                 toolbar.SetTitle(Resource.String.trending_title);
                 SetSupportActionBar(toolbar);
             }
+
+            var bottomNavView = FindViewById<BottomNavigationView>(Resource.Id.main_bottom_nav);
+            bottomNavView?.SetOnNavigationItemSelectedListener(this);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -28,6 +33,21 @@ namespace Showtime.Mob
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            item.SetChecked(true);
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_trending:
+                    return true;
+                case Resource.Id.action_user:
+                    return true;
+            }
+
+            return false;
         }
     }
 }
